263fd9decb31d530fbbe2a2679902f25b1f4b7b2
syaku <syaku@naver.com>
2025-05-27 01:42:25 +0900

    35

---
+++ src/test/java/com/leetcode/problem/SearchInsertPosition35Test.java
@@ -0,0 +1,19 @@
+package com.leetcode.problem;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class SearchInsertPosition35Test {
+    int searchInsert(int[] nums, int target) {
+        log.debug("출력해라 {}", nums[0]);
+        return 0;
+    }
+
+    @Test
+    void test() {
+        System.out.println("good");
+        this.searchInsert(new int[]{1, 3, 5, 6}, 5);
+    }
+}
